90696a4a475df12ac93526c0eb07b8ffaea1d870
Alvaro Santos <alvaro_santos92@hotmail.com>
2016-02-27 21:51:16 +0100

    First version hello world heroku

--- index alvsanmun.js
+++ index alvsanmun.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+var express = require("express");
+
+var app = express();
+
+app.get("/",(req,res) => {
+	res.write("Hello World");
+	res.end();
+});
+
+app.listen(process.env.PORT);
