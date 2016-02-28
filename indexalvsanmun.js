ac369f832751f37fddd285daeddd42ab9afe9dc9
Alvaro Santos <alvaro_santos92@hotmail.com>
2016-02-28 20:52:11 +0100

    Error in the browser

--- indexalvsanmun.js
+++ indexalvsanmun.js
@@ -1,23 +1,13 @@
 var express = require("express");
-var fs = require("fs");
-var contacts = [];
-
-
 var app = express();
 
+
 app.get("/",(req,res) => {
-
-fs.readFile('datos.json','utf8',(err,content)=>{
-	console.log("Data read");
-	contacts = JSON.parse(content);
+	console.log("Hola");
 	res.write("Hola mundo!!");
-	contacts.forEach((contact) =>{
-		res.write("  - " +contact.name+"("+contact.phone+")");
-	});
-
 	res.write("----------");
 	res.end;
-	});	
+
 });
 
-app.listen(10000);
+app.listen(12345);
