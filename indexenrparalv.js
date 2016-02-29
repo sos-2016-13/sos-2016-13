4ab6dbf8a88aaa173fb732047d97236b285a4596
Enrique Pardo Alvarez <enriquezpalvarez@gmail.com>
2016-03-01 00:31:34 +0100

    prueba para sos-2016-13

--- indexenrparalv.js
+++ indexenrparalv.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
 
-app.get("/",(req,res) => {
-console.log("Hello World");
+app.get("/about",(req,res) => {
+res.write("<html><body>________________________Miembros del Grupo________________________<ul>");
+res.write("<li>"+"Alvaro Santos Munoz"+ " ("+"Oil consumed and electricity consumed"+")</li>");
+res.write("<li>"+"Enrique Pardo Alvarez"+ " ("+"Methane emissions, CO2 emissions and nitrous oxide emissions"+")</li>");
+res.write("<li>"+"Eugenio Rodriguez Molina"+ " ("+"Population and electricity access (% for population)"+")</li>");
+res.write("</ul>___________________</body></html>");
 res.end();
 });
 
