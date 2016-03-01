e5aeef7459efe59932bd26e9b1fdefa70bf12877
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-01 14:42:18 +0100

    First work

--- indexenrparalv.js
+++ indexenrparalv.js
@@ -10,4 +10,26 @@ res.write("</ul>___________________</body></html>");
 res.end();
 });
 
+
+app.get("/about/methane-co2-nitrous-emissions",(req,res) => {
+res.write("<li>"+"Enrique Pardo Alvarez"+ " ("+"Methane emissions, CO2 emissions and nitrous oxide emissions"+")</li>");
+
+res.end();
+});
+
+
+app.get("/about/oil-electricity-consumed",(req,res) => {
+res.write("<li>"+"Alvaro Santos Muñoz"+ " ("+"Oil and electricity consumed"+")</li>");
+
+res.end();
+});
+
+app.listen(process.env.PORT);
+
+app.get("/about/population-electricity-access",(req,res) => {
+res.write("<li>"+"Eugenio Jesus Rodriguez Molina"+ " ("+"Population and electricity access"+")</li>");
+
+res.end();
+});
+
 app.listen(process.env.PORT);
