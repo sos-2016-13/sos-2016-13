9340728a91878018f46faa3e89e18e357e8c1e48
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-02 21:04:51 +0100

    Prueba

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use("/about",express.static(__dirname + '/public'));
 
 //app.use("/about/consumed",express.static(__dirname + '/public/consumed'));
 
