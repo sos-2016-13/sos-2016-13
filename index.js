c3491d256e44728820ef179437dac104ea6e2f61
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-02 20:43:21 +0100

    First work

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.use("/about",express.static(__dirname + '/public'));
+
+app.use("/about/consumed",express.static(__dirname + '/public/consumed'));
+
+app.use("/about/emissions",express.static(__dirname + '/public/emissions'));
+
+app.use("/about/population",express.static(__dirname + '/public/population'));
+
+app.listen(process.env.PORT);
