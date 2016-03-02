a78a0d4f16c44c0443ee409dd59c81b88197d026
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-02 21:02:09 +0100

    Prueba

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 
-app.use("/about",express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.use("/about/consumed",express.static(__dirname + '/public/consumed'));
+//app.use("/about/consumed",express.static(__dirname + '/public/consumed'));
 
-app.use("/about/emissions",express.static(__dirname + '/public/emissions'));
+//app.use("/about/emissions",express.static(__dirname + '/public/emissions'));
 
-app.use("/about/population",express.static(__dirname + '/public/population'));
+//app.use("/about/population",express.static(__dirname + '/public/population'));
 
-app.listen(process.env.PORT);
+app.listen(12345);
