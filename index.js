e63591c9954e4b8579a7743a234441b04b97dc44
Enrique Pardo Alvarez <enriquezpalvarez@gmail.com>
2016-03-05 19:25:25 +0100

    prueba about 1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.use("/about",express.static(__dirname + '/static'));
+app.use("/",express.static(__dirname + '/static'));
 
-app.use("/about/consumed",express.static(__dirname + '/static/consumed'));
+app.use("/consumed",express.static(__dirname + '/static/consumed'));
 
-app.use("/about/emissions",express.static(__dirname + '/static/emissions'));
+app.use("/emissions",express.static(__dirname + '/static/emissions'));
 
-app.use("/about/population",express.static(__dirname + '/static/population'));
+app.use("/population",express.static(__dirname + '/static/population'));
 
 app.get("/time",(req,res) => {
  
