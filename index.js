3760efe5e2124a7395590c29fb83448f6f99432d
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-05 19:15:56 +0100

    Fin primera entrega

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var app = express();
 
-app.use("/about",express.static(__dirname + '/public'));
+app.use("/about",express.static(__dirname + '/static'));
 
-app.use("/about/consumed",express.static(__dirname + '/public/consumed'));
+app.use("/about/consumed",express.static(__dirname + '/static/consumed'));
 
-app.use("/about/emissions",express.static(__dirname + '/public/emissions'));
+app.use("/about/emissions",express.static(__dirname + '/static/emissions'));
 
-app.use("/about/population",express.static(__dirname + '/public/population'));
+app.use("/about/population",express.static(__dirname + '/static/population'));
+
+app.get("/time",(req,res) => {
+ 
+ var now = new Date()
+ res.send("<html><body><h1> It is "+ now+"</h1></body></html>");
 
 app.listen(process.env.PORT);
