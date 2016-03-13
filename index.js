d6b21672f5c98abb646ff96e592e8ce611875f0b
Enrique Pardo Alvarez <enriquezpalvarez@gmail.com>
2016-03-13 19:34:59 +0100

    First version good

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var express = require("express");
 var app = express();
-var bodyParser =  require("body-parser");
-
-var port = (process.env.PORT || 10000);
 
 app.use("/",express.static(__dirname + '/static'));
 
@@ -12,21 +9,10 @@ app.use("/emissions",express.static(__dirname + '/static/emissions'));
 
 app.use("/population",express.static(__dirname + '/static/population'));
 
-
-var players = [{ name: "PauGasol"}];
-
-app.get("/api/sandbox/nba",(req,res) => {
-	var name = req.params.name;
-	res.send(players[0]);
-});
-
-
-
-
 app.get("/time",(req,res) => {
  
  var now = new Date()
  res.send("<html><body><h1>It is "+ now+"</h1></body></html>")
 });
 
-app.listen(port);
+app.listen(process.env.PORT);
