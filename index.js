164fcc2433502cd4cfe404c7c1dfebe3cbe5a2a6
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-13 19:58:01 +0100

    prueba

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser =  require("body-parser");
+
+var port = (process.env.PORT || 10000);
 
 app.use("/",express.static(__dirname + '/static'));
 
@@ -9,10 +12,21 @@ app.use("/emissions",express.static(__dirname + '/static/emissions'));
 
 app.use("/population",express.static(__dirname + '/static/population'));
 
+
+var players = [{ name: "PauGasol"}];
+
+app.get("/api/sandbox/nba",(req,res) => {
+	var name = req.params.name;
+	res.send(players[0]);
+});
+
+
+
+
 app.get("/time",(req,res) => {
  
  var now = new Date()
  res.send("<html><body><h1>It is "+ now+"</h1></body></html>")
 });
 
-app.listen(process.env.PORT);
+app.listen(port);
