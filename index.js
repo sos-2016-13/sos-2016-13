e4df8000de573a5b1ad23d0e3650ed287e273333
Enrique Pardo Alvarez <enriquezpalvarez@gmail.com>
2016-03-14 00:06:25 +0100

    api rest.Closes #8

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-  var express = require("express");      
+
+var express = require("express");      
+
 var app = express();
+
 var bodyParser =  require("body-parser");
 
 var port = (process.env.PORT || 10000);
