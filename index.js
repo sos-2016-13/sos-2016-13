066eaf38b6a4015ad5c044cc557c07bd665a0f15
alvsanmun <alvaro_santos92@hotmail.com>
2016-03-13 19:12:20 +0100

    First Version

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser =  require("body-parser");
+
+
 
 app.use("/",express.static(__dirname + '/static'));
 
@@ -8,6 +11,17 @@ app.use("/consumed",express.static(__dirname + '/static/consumed'));
 app.use("/emissions",express.static(__dirname + '/static/emissions'));
 
 app.use("/population",express.static(__dirname + '/static/population'));
+
+
+var players = [{ name: "PauGasol"}];
+
+app.get("/api/sandbox/nba/:name",(req,res) => {
+	var name = req.params.name;
+	res.send(players[0]);
+});
+
+
+
 
 app.get("/time",(req,res) => {
  
