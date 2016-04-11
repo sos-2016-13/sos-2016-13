2d0fd2b0897921a15b2de7801ccb76e56fbc4dea
alvsanmun <alvaro_santos92@hotmail.com>
2016-04-11 19:34:27 +0200

    3 WORK

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser =  require("body-parser");
 
-var port = (process.env.PORT || 10000);
+var port = (process.env.PORT);
 
 app.use("/",express.static(__dirname + '/static'));
 app.use("/RESTClient",express.static(__dirname + '/RESTClient'));
