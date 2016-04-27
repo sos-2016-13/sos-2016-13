f61b8ede7d4b6735402e6cc5d704a58d972e12cb
Eugenio <eu-jrm-94@hotmail.com>
2016-04-27 19:09:04 +0200

    prueba

---
+++ static/data/population/js/population.js
@@ -0,0 +1,32 @@
+ $(document).ready(function (){
+        console.log("JQuery ready");
+      
+          var request = $.ajax({
+              type: "GET",
+              url:'https://sos-2016-13.herokuapp.com/api/v1/population?apikey=eea',
+              data: "{}",
+              contentType: "application/json",
+              dataType: "json",
+              cache: false,
+              success: (data) => {
+              var trHTML = '';
+              $.each(data, function (i, item) {
+              //Cambiar codigo para que muestre los elementos independientes
+                  trHTML += '<tr><td>' + data[i].country + '</td><td>' + data[i].year + '</td><td>'+ data[i].population + '</td><td>' + data[i].access_to_electricity + '</td><td>';
+              });
+
+              $('#location').append(trHTML);
+              console.log(trHTML);
+              }
+            });
+            request.done((data)=>{
+              console.log("data received");
+              console.log(data);
+              //$("#location").text(JSON.stringify(data));
+            });
+            request.always((jqXHR,status)=>{
+                if(status=="error")
+                console.log("Status: "+ jqXHR.status);
+            });
+       
+    });
