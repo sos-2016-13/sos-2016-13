3302d377dc36eb2e9c625ab819d5b1f49dbdefa7
alvsanmun <alvaro_santos92@hotmail.com>
2016-05-14 19:22:47 +0200

    Proxy and SLA

--- index.js
+++ index.js
@@ -43,15 +43,15 @@ app.use(pathsAlvaro, function(req,res){
 
 
 
-
 /*
+
 app.use(function(req, res, next) {
 res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
-
+*/
 //---------------------
 app.use(bodyParser.json());
 //---------------------
